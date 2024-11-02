a4cb98eacc01808fc3abe9c2a414058d1284d8ea
shaikmadharbee <146852396+shaikmadharbee@users.noreply.github.com>
2024-11-02 12:22:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>HELOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</h1>
+      <h1>CHINNIIIIIIIIII</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
