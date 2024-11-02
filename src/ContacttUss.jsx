319e3597845c3e53daef4c5ddc8779974c8534ed
unknown <shaikmadharbee2609@gmail.com>
2024-11-02 08:16:43 +0530

    this is first commit

---
+++ src/ContacttUss.jsx
@@ -0,0 +1,10 @@
+function ContacttUss()
+{
+    return(
+        <>
+        <h1>This is ContactUs page</h1>
+        <p>Welcome</p>
+        </>
+    )
+}
+export default ContacttUss;
---
+++ src/Greetingg.jsx
@@ -0,0 +1,9 @@
+function Greetingg()
+{
+    return(
+    <>
+    <h1>HELLOOOOOOOOOOOOOOOOOOOOo</h1>
+    </>
+    )
+}
+export default Greetingg;
